141d8ad7a2e212c624e3f86a7871761cba4d372d
Ian Brown <ibapps39@gmail.com>
2025-07-05 18:59:36 -0600

    cross product, unfinished!

--- include/test.h
+++ include/test.h
@@ -88,3 +88,63 @@ void validate_dot(std::vector<vec3> &A, std::vector<vec3> &B, std::vector<vec3> 
     }
     std::cout << "Correct: " << correct << "/" << N << std::endl;
 }
+
+void error_div(std::vector<vec3> &A, std::vector<vec3> &B, std::vector<vec3> &C, size_t i)
+{
+    std::cout << "Incorrect: C[" << i << "]:" << "[" << C[i] << "]" << std::endl;
+    std::cout << "\t A[" << i << "]:" << A[i] << std::endl;
+    std::cout << "\t B[" << i << "]:" << B[i] << std::endl;
+    std::cout << "\t C[" << i << "] should be:[" << A[i].x() / B[i].x() << ", " << (A[i].y() / B[i].y()) << ", "
+              << (A[i].z() / B[i].z()) << "]"
+              << std::endl;
+}
+void validate_div(std::vector<vec3> &A, std::vector<vec3> &B, std::vector<vec3> &C, size_t N)
+{
+    size_t correct = 0;
+    size_t i = 0;
+    for (; i < N; ++i)
+    {
+        correct += (C[i].x() == (A[i].x() / B[i].x()) &&
+                    C[i].y() == (A[i].y() / B[i].y()) &&
+                    C[i].z() == (A[i].z() / B[i].z()));
+    }
+    if (correct < i) { 
+        error_dot(A, B, C, i); 
+    }
+    std::cout << "Correct: " << correct << "/" << N << std::endl;
+}
+
+void error_cross(std::vector<vec3> &A, std::vector<vec3> &B, std::vector<vec3> &C, size_t i)
+{
+
+    // A[i].y() * B[i].z() - A[i].z() * B[i].y(),
+    // A[i].z() * B[i].x() - A[i].x() * B[i].z(),
+    // A[i].x() * B[i].y() - A[i].y() * B[i].x()
+
+    std::cout << "Incorrect: C[" << i << "]:" << "[" << C[i] << "]" << std::endl;
+    std::cout << "\t A[" << i << "]:" << A[i] << std::endl;
+    std::cout << "\t B[" << i << "]:" << B[i] << std::endl;
+    std::cout << "\t C[" << i << "] should be:[" << 
+    ((A[i].y() * B[i].z()) - A[i].z() * B[i].y()) 
+    << ", " << 
+    A[i].z() * B[i].x() - A[i].x() * B[i].z() 
+    << ", " << 
+    (A[i].x() * B[i].y() - A[i].y() * B[i].x()) 
+    << "]"
+    << std::endl;
+}
+void validate_cross(std::vector<vec3> &A, std::vector<vec3> &B, std::vector<vec3> &C, size_t N)
+{
+    size_t correct = 0;
+    size_t i = 0;
+    for (; i < N; ++i)
+    {
+        correct += (C[i].x() == ((A[i].y() * B[i].z()) - A[i].z() * B[i].y()) &&
+                    C[i].y() == ((A[i].z() * B[i].x()) - A[i].x() * B[i].z()) &&
+                    C[i].z() == ((A[i].x() * B[i].y()) - A[i].y() * B[i].x()));
+    }
+    if (correct < i) { 
+        error_cross(A, B, C, i); 
+    }
+    std::cout << "Correct: " << correct << "/" << N << std::endl;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -53,14 +53,14 @@ int main()
     std::cout << std::endl;
     std::fill(C.begin(), C.end(), vec3(0.00, 0.00, 0.00f));
 
-    double neon_threads = timeFunction(thread_neon, N, '+', m_threads, A, B, C);
+    double neon_threads = timeFunction(thread_neon_switch, N, '+', m_threads, A, B, C);
     std::printf("Time for neon_threads (void) with %i elements: %fs\n", N, neon_threads);
     validate_sum(A, B, C, N);
     std::cout << std::endl;
     std::fill(C.begin(), C.end(), vec3(0.00, 0.00, 0.00f));
 
     auto begin = std::chrono::steady_clock::now();
-    thread_neon_generic(neon_add, std::ref(A), std::ref(B), std::ref(C));
+    thread_neon(neon_add, std::ref(A), std::ref(B), std::ref(C));
     auto end = std::chrono::steady_clock::now();
     double neon_threads_generic_time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000000.0;
     std::printf("Time for neon_threads_generic (void), adding, with %i elements: %fs\n", N, neon_threads_generic_time);
@@ -69,7 +69,7 @@ int main()
     std::fill(C.begin(), C.end(), vec3(0.00, 0.00, 0.00f));
     
     begin = std::chrono::steady_clock::now();
-    thread_neon_generic(neon_sub, std::ref(A), std::ref(B), std::ref(C));
+    thread_neon(neon_sub, std::ref(A), std::ref(B), std::ref(C));
     end = std::chrono::steady_clock::now();
     neon_threads_generic_time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000000.0;
     std::printf("Time for neon_threads_generic (void), subtracting, with %i elements: %fs\n", N, neon_threads);
@@ -78,7 +78,7 @@ int main()
     std::fill(C.begin(), C.end(), vec3(0.00, 0.00, 0.00f));
 
     begin = std::chrono::steady_clock::now();
-    thread_neon_generic(neon_dot, std::ref(A), std::ref(B), std::ref(C));
+    thread_neon(neon_dot, std::ref(A), std::ref(B), std::ref(C));
     end = std::chrono::steady_clock::now();
     neon_threads_generic_time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000000.0;
     std::printf("Time for neon_threads_generic (void), dot, with %i elements: %fs\n", N, neon_threads);
@@ -86,7 +86,29 @@ int main()
     std::cout << std::endl;
     std::fill(C.begin(), C.end(), vec3(0.00, 0.00, 0.00f));
 
-    
+    begin = std::chrono::steady_clock::now();
+    thread_neon(neon_div, std::ref(A), std::ref(B), std::ref(C));
+    end = std::chrono::steady_clock::now();
+    neon_threads_generic_time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000000.0;
+    std::printf("Time for neon_threads_generic (void), divide, with %i elements: %fs\n", N, neon_threads);
+    validate_div(A, B, C, N);
+    std::cout << std::endl;
+    std::fill(C.begin(), C.end(), vec3(0.00, 0.00, 0.00f));
+
+    begin = std::chrono::steady_clock::now();
+    thread_neon(neon_cross, std::ref(A), std::ref(B), std::ref(C));
+    end = std::chrono::steady_clock::now();
+    neon_threads_generic_time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000000.0;
+    std::printf("Time for neon_threads_generic (void), cross, with %i elements: %fs\n", N, neon_threads);
+    //validate_cross(A, B, C, N);
+    std::cout << std::endl;
+    read_vec(A, B, C);
+    for(int i=0; i < 10; i++)
+    {
+        vec3 C = cross(A[i],B[i]);
+        std::cout << C << std::endl;
+    }
+    //std::fill(C.begin(), C.end(), vec3(0.00, 0.00, 0.00f));
 
     std::cout << "\n//END////END////END////END////END////END////END//\n";
     return 0;
